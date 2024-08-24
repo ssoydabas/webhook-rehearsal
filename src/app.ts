7c9d490e20eee0a799de4bb7e78d568420d2e124
Sertan Soydabas <sertan.soydabas@dt.net.tr>
2024-08-25 01:46:44 +0300

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response } from "express";
+import { port, nodeEnv } from "./config";
+
+const app = express();
+console.log(`Environment: ${nodeEnv}`);
+console.log(`Port: ${port}`);
+
+app.use(express.json());
+
+app.get("/hello-world", (req: Request, res: Response) => {
+  res.json({ message: "Hello, World!" });
+});
+
+app.post("/echo-message", (req: Request, res: Response) => {
+  if (!req.body.message) {
+    return res.status(400).json({ error: "Message is required" });
+  }
+
+  res.json(req.body.message);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
---
+++ src/config.ts
@@ -0,0 +1,6 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const nodeEnv = process.env.NODE_ENV;
+export const port = process.env.PORT || 3000;
